85e85d8dfe4de903559742abe53e434c587991c3
DigitalzonePC <linux68@digitalzone.co.kr>
2021-02-02 16:46:18 +0900

    javascript 31강

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -1,3 +1,22 @@
+//Ex5 : 엘리먼트 노드의 속성  & CSS 속성 변경
+window.addEventListener("load", function() {
+    var section = document.querySelector("#section5");
+    var srcInput = section.querySelector(".src-input");
+    var imgSelect = section.querySelector(".img-select");
+    var changeButton = section.querySelector(".change-button");
+    var img = section.querySelector(".img");
+    var colorInput = section.querySelector(".color-input");
+
+    changeButton.onclick = function() {
+        img.src = "images/"+srcInput.value;
+        // img.src = "images/"+imgSelect.value;
+
+        // img.style["border-color"] = colorInput.value;
+        img.style.borderColor = colorInput.value;
+        console.log(img.className);
+    }
+});
+
 //Ex4 : childNodes를 이용한 노드선택
 window.addEventListener("load", function() {
     var section4 = document.querySelector("#section4");
