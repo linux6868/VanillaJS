01bc95b413140392293e3e6aaf58e2d499d31367
DigitalzonePC <linux68@digitalzone.co.kr>
2021-02-05 16:55:17 +0900

    34강

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -1,23 +1,41 @@
 //Ex7-노드복제와 템플릿 태그
 window.addEventListener("load", function() {
-    var notice = [
+    var notices = [
         {id:5, title:"쫘이야~~~", regDate:"2019-01-26", writeId:"nerwlec", hit:0},
         {id:6, title:"나 좀 복제해줘~", regDate:"2019-01-26", writeId:"nerwlec", hit:17}
     ];
 
     var section = document.querySelector("#section7");
 
-    var noticeList = section.querySelectot(".notice-list");
+    var noticeList = section.querySelector(".notice-list");
     var tbodyNode = noticeList.querySelector("tbody");
     var cloneButton = section.querySelector(".clone-button");
     var templateButton = section.querySelector(".template-button");
 
     cloneButton.onclick = function() {
+        var trNode = noticeList.querySelector("tbody tr");
+        var cloneNode = trNode.cloneNode(true);
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        tds[1].innerHTML = '<a href="'+notices[0].id+'">'+notices[0].title+'</a>';
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writeId;
+        tds[4].textContent = notices[0].hit;
 
+        tbodyNode.appendChild(cloneNode);
     };
 
     templateButton.onclick = function() {
+        var template = section.querySelector("template");
+        var cloneNode = template.cloneNode(true);
+        var tds = cloneNode.querySelectorAll("td");
+        tds[0].textContent = notices[0].id;
+        tds[1].innerHTML = '<a href="'+notices[0].id+'">'+notices[0].title+'</a>';
+        tds[2].textContent = notices[0].regDate;
+        tds[3].textContent = notices[0].writeId;
+        tds[4].textContent = notices[0].hit;
 
+        tbodyNode.appendChild(cloneNode);
     };
 });
 
