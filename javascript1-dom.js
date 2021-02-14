2cf49fb3d194327b7ba4811e5f1dfc810a0e9dc0
LeeJongGu <linux68@hanmail.net>
2021-02-14 22:36:31 +0900

    자바스크립트 & DOM 프로그래밍 39강

--- javascript1-dom.js
+++ javascript1-dom.js
@@ -1,3 +1,144 @@
+//Ex10-클릭한 컬럼을 기준으로 레코드 정렬하기
+window.addEventListener("load", function() {
+    var notices = [
+        {"id":1, "title":"유투브에 끌려다니지 않으려고 했는데....ㅜㅜ..", "regDate":"2019-02-05", "writerId":"newlec", "hit":2},
+        {"id":2, "title":"자바스크립트란..", "regDate":"2019-02-02", "writerId":"newlec", "hit":0},
+        {"id":3, "title":"기본기가 튼튼해야....", "regDate":"2019-02-01", "writerId":"newlec", "hit":1},
+        {"id":4, "title":"근데 조회수가 ㅜㅜ..", "regDate":"2019-01-25", "writerId":"newlec", "hit":0}
+    ];
+
+    var section = document.querySelector("#section10");
+
+    var noticeList = section.querySelector(".notice-list");
+    var titldTd = section.querySelector(".title");
+    var tbodyNode = noticeList.querySelector("tbody");
+
+    var bindData = function() {
+        var template = section.querySelector("template");
+
+        for(var i=0; i<notices.length; i++) {
+            var cloneNode = document.importNode(template.content, true);
+            var tds = cloneNode.querySelectorAll("td");
+
+            tds[0].textContent = notices[i].id;
+    
+            var aNode = tds[1].children[0];
+            aNode.href = notices[i].id;
+            aNode.textContent = notices[i].title;
+    
+            tds[2].textContent = notices[i].regDate;
+            tds[3].textContent = notices[i].writeId;
+            tds[4].textContent = notices[i].hit;
+    
+            tbodyNode.appendChild(cloneNode);
+        }
+    };
+
+    bindData();
+
+    var titleSorted = false;
+
+    titldTd.onclick = function() {
+        tbodyNode.innerHTML = "";
+
+        if (!titleSorted) {
+            titleSorted = true;
+
+            notices.sort(function(a, b) {
+                if (a.title < b.title) return -1;
+                else if (a.title > b.title) return 1;
+                else return 0;
+            });
+        }
+        else
+            notices.reverse();
+
+        bindData();
+    };
+});
+
+//Ex9-다중 노드선택 방법과 일괄삭제, 노드의 자리바꾸기
+window.addEventListener("load", function() {
+
+    var section = document.querySelector("#section9");
+
+    var noticeList = section.querySelector(".notice-list");
+    var tbody = noticeList.querySelector("tbody");
+    var allCheckbox = section.querySelector(".overall-checkbox");
+    var delButton = section.querySelector(".del-button");
+    var swapButton = section.querySelector(".swap-button");
+
+    allCheckbox.onchange = function() {
+        var inputs = tbody.querySelectorAll("input[type='checkbox']");
+        for (var i=0; i<inputs.length; i++)
+            inputs[i].checked = allCheckbox.checked;
+    };
+
+    delButton.onclick = function() {
+        var inputs = tbody.querySelectorAll("input[type='checkbox']:checked");
+        for(var i=0; i<inputs.length; i++)
+            inputs[i].parentElement.parentElement.remove();
+    };
+
+    swapButton.onclick = function() {
+        var inputs = tbody.querySelectorAll("input[type='checkbox']:checked");
+        if (inputs.length != 2){
+            alert("엘리먼트는 2개를 선택해야만 합니다.");
+            return;
+        }
+
+        var trs = [];
+        for(var i=0; i<inputs.length; i++)
+            trs.push(inputs[i].parentElement.parentElement);
+        
+        var cloneNode = trs[0].cloneNode(true);
+        trs[1].replaceWith(cloneNode);
+        trs[0].replaceWith(trs[1]);
+
+    };
+});
+
+//Ex8-노드 삽입과 바꾸기
+window.addEventListener("load", function() {
+
+    var section = document.querySelector("#section8");
+
+    var noticeList = section.querySelector(".notice-list");
+    var tbodyNode = noticeList.querySelector("tbody");
+    var upButton = section.querySelector(".up-button");
+    var downButton = section.querySelector(".down-button");
+
+    var currentMode = tbodyNode.firstElementChild; //.children[0]
+
+    downButton.onclick = function() {
+        var nextNode = currentMode.nextElementSibling;
+
+        if(nextNode == null) {
+            alert("더 이상 이동할 수 없습니다.");
+            return;
+        }
+
+        // tbodyNode.removeChild(nextNode);    //insertBefore 는 자동삭제
+        // tbodyNode.insertBefore(nextNode, currentMode);
+        currentMode.insertAdjacentElement("beforebegin", nextNode);
+
+    };
+
+    upButton.onclick = function() {
+        var prevNode = currentMode.previousElementSibling;
+
+        if (prevNode == null) {
+            alert("더 이상 이동할 수 없습니다.");
+            return;
+        }
+
+        // tbodyNode.removeChild(currentMode);
+        // tbodyNode.insertBefore(currentMode, prevNode);
+        currentMode.insertAdjacentElement("afterend", prevNode);
+
+    };
+});
+
 //Ex7-노드복제와 템플릿 태그
 window.addEventListener("load", function() {
     var notices = [
@@ -27,15 +168,25 @@ window.addEventListener("load", function() {
 
     templateButton.onclick = function() {
         var template = section.querySelector("template");
-        var cloneNode = template.cloneNode(true);
-        var tds = cloneNode.querySelectorAll("td");
-        tds[0].textContent = notices[0].id;
-        tds[1].innerHTML = '<a href="'+notices[0].id+'">'+notices[0].title+'</a>';
-        tds[2].textContent = notices[0].regDate;
-        tds[3].textContent = notices[0].writeId;
-        tds[4].textContent = notices[0].hit;
 
-        tbodyNode.appendChild(cloneNode);
+        for(var i=0; i<notices.length; i++) {
+            var cloneNode = document.importNode(template.content, true);
+            // var cloneNode = template.cloneNode(true);
+            var tds = cloneNode.querySelectorAll("td");
+
+            tds[0].textContent = notices[i].id;
+            // tds[1].innerHTML = '<a href="'+notices[i].id+'">'+notices[i].title+'</a>';
+    
+            var aNode = tds[1].children[0];
+            aNode.href = notices[i].id;
+            aNode.textContent = notices[i].title;
+    
+            tds[2].textContent = notices[i].regDate;
+            tds[3].textContent = notices[i].writeId;
+            tds[4].textContent = notices[i].hit;
+    
+            tbodyNode.appendChild(cloneNode);
+        }
     };
 });
 
