70fc5e21f0593ebaf3eaba07f43280ec41a2f531
DigitalzonePC <linux68@digitalzone.co.kr>
2021-02-24 17:12:34 +0900

    45강

--- javascript2-event.js
+++ javascript2-event.js
@@ -1,3 +1,33 @@
+
+// Ex4-서로 다른 기능의 여러버튼을 가진 화면에서 이벤트를 처리하는 방법
+window.addEventListener("load", function(){
+    var section = document.querySelector("#section4");
+    var tbody = section.querySelector(".notice-list tbody");
+
+    tbody.onclick = function(e) {
+        e.preventDefault();
+
+        var target = e.target;
+        if(target.nodeName != "A") 
+            return;
+
+        if(target.classList.contains("sel-button")) {
+            var tr = target.parentElement;
+
+            for( ; tr.nodeName != "TR"; tr=tr.parentElement );
+            tr.style.background = "yellow";
+
+        } else if(target.classList.contains("edit-button")) {
+
+        } else if(target.classList.contains("del-button")) {
+            // var tr = e.target.parentElement.parentElement;
+            // tr.remove();
+        }
+
+    };
+}); 
+
+
 // Ex3-이벤트 버블링 멈추기
 window.addEventListener("load", function(){
 
@@ -8,11 +38,19 @@ window.addEventListener("load", function(){
     var currentImg = section.querySelector(".current-img");
     
     imgList.onclick = function(e){
-        
+        console.log("imgList.onclick");
+        if(e.target.nodeName != "IMG") return;
+        currentImg.src = e.target.src;
     };
 
     addButton.onclick = function(e){
-        
+        e.stopPropagation();
+
+        console.log("addButton.onclick");
+        var img = document.createElement("img");
+        img.src = "images/img1.jpg";
+
+        currentImg.insertAdjacentElement("afterend", img);
     };
 
 }); 
@@ -29,16 +67,36 @@ window.addEventListener("load", function(){
     var currentImg = section.querySelector(".current-img");
     
     imgList.onclick = function(e){
-        
+        if(e.target.nodeName != "IMG") return;
+
+        currentImg.src = e.target.src;
     };
 }); 
 
 
 //연습문제 1-선택된 레코드 삭제하기:event target
 window.addEventListener("load", function(){
+    var section = document.querySelector("#section1-1");
+    var bodyList = section.querySelector(".body-list");
 
-    
+    bodyList.onclick = function(e) {
+        if(e.target.nodeName != "INPUT") return;
+
+        var tr = e.target.parentElement.parentElement;
+        tr.remove();
+    };
 }); 
+
+// window.addEventListener("load", function(){
+//     var section = document.querySelector("#section1-1");
+//     var delButtons = section.querySelectorAll(".del-button");
+
+//     for(var i=0; i<delButtons.length; i++ ) 
+//         delButtons[i].onclick = function(e) {
+//             var tr = e.target.parentElement.parentElement;
+//             tr.remove();
+//         };
+// }); 
 
 
 
@@ -49,6 +107,10 @@ window.addEventListener("load", function(){
     
     var imgs = section.querySelectorAll(".img");
     var currentImg = section.querySelector(".current-img");
-    
+
+    for(var i=0; i<imgs.length; i++ )
+        imgs[i].onclick = function(e) {
+            currentImg.src = e.target.src;
+        };
 
 }); 
